62814a0466871f96f3b80478a92b0fb6c2360e3a
Misa-Kanuma <misa.kanuma@gmail.com>
2023-01-22 22:27:59 +0900

    Navigation

--- app/src/main/java/com/bambi/studyenglishapp/MainActivity.kt
+++ app/src/main/java/com/bambi/studyenglishapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.bambi.studyenglishapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.bambi.studyenglishapp.databinding.ActivityMainBinding
+import com.bambi.studyenglishapp.databinding.FragmentMenuBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- app/src/main/java/com/bambi/studyenglishapp/WordBookFragment.kt
+++ app/src/main/java/com/bambi/studyenglishapp/WordBookFragment.kt
@@ -5,28 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.findNavController
 import com.bambi.studyenglishapp.databinding.FragmentMenuBinding
+import com.bambi.studyenglishapp.databinding.FragmentWordBookBinding
 
-class MenuFragment : Fragment() {
+class WordBookFragment : Fragment() {
 
-    private lateinit var binding: FragmentMenuBinding
+    private lateinit var binding: FragmentWordBookBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        super.onCreate(savedInstanceState)
-        binding = FragmentMenuBinding.inflate(layoutInflater)
+        binding = FragmentWordBookBinding.inflate(layoutInflater)
         val view = binding.root
 
-        binding.wordBookButton.setOnClickListener {
-            findNavController().navigate(R.id.action_menuFragment_to_wordBookFragment)
+        binding.homeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_wordBookFragment_to_menuFragment)
 
         }
 
         return view
     }
+
 
 }
