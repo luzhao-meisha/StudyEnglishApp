cb6b8c3d021b107a3af9950e3e3471e8ba1fc49d
Misa-Kanuma <misa.kanuma@gmail.com>
2023-05-20 02:17:46 +0900

    CheckFragment 問題生成の処理を追加

--- app/src/main/java/com/bambi/studyenglishapp/CheckFragment.kt
+++ app/src/main/java/com/bambi/studyenglishapp/CheckFragment.kt
@@ -13,7 +13,6 @@ import com.bambi.studyenglishapp.databinding.FragmentCheckBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlin.properties.Delegates
 import kotlin.random.Random
 
 class CheckFragment : Fragment() {
@@ -54,19 +53,63 @@ class CheckFragment : Fragment() {
         }
     }
 
+    /**
+     * 問題を作成する
+     * */
     private suspend fun displayQuiz() {
         withContext(Dispatchers.IO) {
 
             //データ数取得
             val size = wordDataDao.count()
             Log.d("レコードの総数", size.toString())
+
+
+            val randomNumberList = mutableListOf<Int>()
+
+            /**ランダムに問題となる単語とその意味を取得*/
             //ランダムな数を生成
-            val random = size.let { Random.nextInt(1, it) }
-            //ランダムに単語を表示
-            val englishName = wordDataDao.getEnglishNameById(random)
+            var random = size.let { Random.nextInt(1, it) }
+            randomNumberList.add(random)
+            //英単語
+            val englishWord = wordDataDao.getEnglishNameById(random)
+            //意味
+            val japaneseWordList = mutableListOf<String>()
+            japaneseWordList.add(wordDataDao.getJapaneseNameById(random))
+
+            /**ランダムに意味を取得*/
+            for (i in 0..2) {
+                random = size.let { Random.nextInt(1, it) }
+                randomNumberList.add(random)
+                japaneseWordList.add(wordDataDao.getJapaneseNameById(random))
+            }
+
+            // 選択肢4つのButtonリスト作成
+            val selectionList = mutableListOf(
+                binding.word0,
+                binding.word1,
+                binding.word2,
+                binding.word3,
+            )
+
 
             withContext(Dispatchers.Main) {
-                binding.word.text = englishName
+                binding.word.text = englishWord
+
+                //重複を許さずに要素を選択するために、ランダムなインデックスのリストを作成し
+                val uniqueIndices = mutableListOf<Int>()
+                while (uniqueIndices.size < selectionList.size) {
+                    val randomIndex = Random.nextInt(japaneseWordList.size)
+                    if (uniqueIndices.contains(randomIndex).not()) {
+                        uniqueIndices.add(randomIndex)
+                    }
+                }
+
+                // ランダムに選ばれた文字列をセット
+                for (i in selectionList.indices) {
+                    val randomIndex = uniqueIndices[i]
+                    val randomWord = japaneseWordList[randomIndex]
+                    selectionList[i].text = randomWord
+                }
             }
         }
     }
